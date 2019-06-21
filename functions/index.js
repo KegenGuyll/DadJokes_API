d9781d9df3ad4efcb31b24c35029ee2f1d64338a
Kegen Guyll <guyllkegen@gmail.com>
2019-06-21 18:05:04 -0500

    first commit

---
+++ functions/index.js
@@ -0,0 +1,68 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const util = require('util');
+const { randomJoke, randomTen, jokeByType } = require('./handler');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+const app = express();
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Try /random_joke, /random_ten, /jokes/random, or /jokes/ten');
+});
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+app.get('/random_joke', (req, res) => {
+  res.json(randomJoke());
+});
+
+app.get('/random_ten', (req, res) => {
+  res.json(randomTen());
+});
+
+app.get('/jokes/random', (req, res) => {
+  res.json(randomJoke());
+});
+
+app.get('/jokes/ten', (req, res) => {
+  res.json(randomTen());
+});
+
+app.get('/jokes/:type/random', (req, res) => {
+  res.json(jokeByType(req.params.type, 1));
+});
+
+app.get('/jokes/:type/ten', (req, res) => {
+  res.json(jokeByType(req.params.type, 10));
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    type: 'error',
+    message: err.message
+  });
+});
